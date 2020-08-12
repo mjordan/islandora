4fbf4f9c50472ff3028147dc60407572fdd53de0
Mark Jordan <mjordan@sfu.ca>
2020-08-12 03:59:19 +0000

    Work in #1491, incorporating @jordandukart 's suggestion.

--- js/image_media_form_defaults.js
+++
@@ -1,6 +0,0 @@
-(function (Drupal, $) {
-  var ImageName = $('input[id=edit-name-0-value]').val();
-  $(document).ajaxSuccess(function () {
-    $('input[data-drupal-selector=edit-field-media-image-0-alt]').val(ImageName);
-  });
-})(Drupal, jQuery);
